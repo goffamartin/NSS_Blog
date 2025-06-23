0b52229c57a23c689ca34d868cb34156ec746488
Goffa Martin <goffamartin@gmai.com>
2025-06-23 22:50:03 +0200

    Core ElastisService implemented, with functional Minimal API endpoint for search and autocomplete

--- Blog.AppHost/Program.cs
+++ Blog.AppHost/Program.cs
@@ -1,6 +1,16 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
+
+var elasticsearch = builder.AddElasticsearch("elasticsearch")
+    .WithDataVolume();
+
+var elasticservice = builder.AddProject<Projects.Blog_ElasticService>("elasticservice")
+    .WithReference(elasticsearch)
+    .WaitFor(elasticsearch)
+    .WithExternalHttpEndpoints();
 
 var apiService = builder.AddProject<Projects.Blog_ApiService>("apiservice");
 
@@ -9,6 +19,9 @@ builder.AddProject<Projects.Blog_Web>("webfrontend")
     .WithReference(cache)
     .WaitFor(cache)
     .WithReference(apiService)
-    .WaitFor(apiService);
+    .WaitFor(apiService)
+    .WithReference(elasticservice)
+    .WaitFor(elasticservice);
+
 
 builder.Build().Run();
---
+++ Blog.ElasticService/Dtos/ArticleSearchDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Blog.ElasticService.Dtos
+{
+    public class ArticleSearchDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public List<string> Tags { get; set; } = new();
+        public string Category { get; set; } = "";
+        public string Author { get; set; } = "";
+        public int AuthorId { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
